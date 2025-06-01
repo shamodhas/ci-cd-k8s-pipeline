109d1b7a48f2608f7b608d17a8979be5a5718e90
shamodhas <shamodha7@gmail.com>
2025-06-01 08:19:43 +0530

    initial deployment

---
+++ src/main/java/lk/ijse/pipeline/api/PipelineController.java
@@ -0,0 +1,26 @@
+package lk.ijse.pipeline.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * --------------------------------------------
+ * Author: Shamodha Sahan
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.com
+ * --------------------------------------------
+ * Created: 6/1/2025 8:15 AM
+ * Project: ci-cd-k8s-pipeline
+ * --------------------------------------------
+ **/
+
+@RestController
+@RequestMapping("/api/v1/app")
+public class PipelineController {
+
+    @GetMapping("/pipeline")
+    public String get() {
+        return "Pipeline working";
+    }
+}
---
+++ src/test/java/lk/ijse/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
